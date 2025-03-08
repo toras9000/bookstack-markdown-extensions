1591b3c3c14c241c3fbc41a9d92405eaa08488b2
toras9000 <toras9000@example.com>
2025-03-08 21:41:16 +0900

    customize

--- docker/assets/template/themes/my-theme/layouts/base.blade.php
+++ docker/assets/template/themes/my-theme/layouts/base.blade.php
@@ -35,6 +35,10 @@
     <!-- OpenSearch -->
     <link rel="search" type="application/opensearchdescription+xml" title="{{ setting('app-name') }}" href="{{ url('/opensearch.xml') }}">
 
+    <!-- Custom theme -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it-footnote/4.0.0/markdown-it-footnote.min.js" nonce="{{ $cspNonce }}" integrity="sha512-giw4jyv75ngVba/TWbP9ouANTIA67sJoNWUvOW8ogr1NqByolnc+jbIwj+yAJ0eF/5SLfwv8IZoIVu8bN5XWRw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="/theme/my-theme/md-it-setup.js" nonce="{{ $cspNonce }}"></script>
+
     <!-- Custom Styles & Head Content -->
     @include('layouts.parts.custom-styles')
     @include('layouts.parts.custom-head')
---
+++ readme.md
@@ -0,0 +1,12 @@
+# BookStack Customize - Markdown Editor extensions
+
+This uses a logical theme to add customization to the Markdown editor.  
+In this example, we specifically add the following  
+
+- Customization to CommonMark parser
+    - Add FootnoteExtension
+    - Add AutolinkExtension
+    - Add DisallowedRawHtmlExtension
+- Customization to markdown-it front-end previewer
+    - Footnote formatting
+    - Autolink enable
